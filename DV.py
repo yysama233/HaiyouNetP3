127ac34a24055da782994c3c406682404c1a90f9
Yang <yysama95@gmail.com>
2017-04-06 15:47:38 -0400

    Jiazhuangxiele

---
+++ DV.py
@@ -0,0 +1,10 @@
+from string import *
+
+class Router(object):
+	def __init__(self,name,AdjList):
+		self.name = name
+		self.AdjList = AdjList
+		self.routes = {name:0}
+	def distance(self,dest):
+		
+class 
