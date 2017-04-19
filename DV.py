e451d4946224bd420a16597c06828ad1fa2c5384
mxie33 <mxie33@gatech.edu>
2017-04-19 19:30:54 -0400

    updates!

--- DV.py
+++ DV.py
@@ -223,45 +223,77 @@ def iniEvent(eventFile):
 #     self.new_update_table(update_dv_list, iteration)
 
 # calculateDV method to calculate a single source routers distance vectors
-def calculateDV(netWorkbasic,srcRouter, poison=False, split=False):
+def calculateDV(netWork,srcRouter, poison=False, split=False):
     Overallchanged = False
-    for destNum,destRouter in netWorkbasic.items():
-        localChanged = False
+    for destNum,destRouter in netWork.items():
+        if destNum == srcRouter.num:
+            continue
         nextHop,minCost = srcRouter.destTable[destNum]
+        tempCost = float('inf')
+
         # check all neighbours if they can reach destination
         for adjNum in srcRouter.AdjList:
             adjCost = srcRouter.costList[adjNum]
-            adjDest = netWorkbasic[adjNum].destTable[destNum][1]
+            adjDest = netWork[adjNum].destTable[destNum][1]
+            #print netWork[adjNum].destTable[destNum]
+            adjNexthop = netWork[adjNum].destTable[destNum][0]
+            #print "adjNextHop:", adjNexthop, "src:", srcRouter.num
+            if netWork[adjNum].destTable[destNum][0] == srcRouter.num:
+                if poison:
+                    print "poison!", "src:", srcRouter.num, "nexthop:", adjNum,"dest:", destNum, "new distance:", disFromAdj
+                    adjDest = float('inf')
+                elif split:
+                    print "split!", "src:", srcRouter.num, "nexthop:", adjNum,"dest:", destNum, "new distance:", disFromAdj
+                    # if split just don't send anything to that router
+                    continue
+                else:
+                    pass
+
+            if split and netWork[adjNum].destTable[destNum][0] == srcRouter.num:
+                print "wrong way, should not go here"
+            if poison and netWork[adjNum].destTable[destNum][0] == srcRouter.num and adjDest != float('inf'):
+                print "wrong poison reverse value"
+
             disFromAdj = adjDest + adjCost
-            if disFromAdj < minCost:
-                print "update!", "src:", srcRouter.num, "nexthop:", adjNum,"dest:", destNum, "new distance:", disFromAdj
-                minCost = disFromAdj
-                nextHop = adjNum
-                Overallchanged = True
-                localChanged = True
+
+            # finding the minimum among all dv from src to dest through different hops
+            if disFromAdj < tempCost:
+                tempCost = disFromAdj
+                if adjNum == destNum:
+                    nextHop = 0
+                else:
+                    nextHop = adjNum
+
+        if tempCost != minCost:
+            print "update!", "src:", srcRouter.num, "nexthop:", adjNum,"dest:", destNum, "new distance:", tempCost
+            Overallchanged = True
+            srcRouter.updateDest(destNum,nextHop,tempCost)
 
         if minCost > 100:
             print "Count to infinity problem! End iteration."
             sys.exit()
-        if localChanged:
-            srcRouter.updateDest(destNum,nextHop,minCost)
-    return Overallchanged
 
+    return Overallchanged
 
 # choosing a routing protocol
 # default is not recalculate and is basic protocol
-def routingProtocol(netWorkbasic, numIterations, basicF, flag, recalculate=False, poison=False, split=False):
+def routingProtocol(netWork, numIterations, basicF, flag,poison=False, split=False):
     #basic if poison == false and split == false
     Overallchanged = False
-
+    if not (poison or split):
+        print "basic protocol"
+    elif(poison):
+        print "poison reversed protocol"
+    else:
+        print "split horizon protocol"
     if flag == 1:
-        printNet(netWorkbasic,numIterations,basicF)
+        printNet(netWork,numIterations,basicF)
 
     # iteration all source router
     print "--------------------------protocol start--------------------------"
-    for srcNum,srcRouter in netWorkbasic.items():
+    for srcNum,srcRouter in netWork.items():
         # check distance vector for every destination
-        localChanged = calculateDV(netWorkbasic,srcRouter)
+        localChanged = calculateDV(netWork,srcRouter,poison=poison,split=split)
         if localChanged:
             Overallchanged = True
     print "--------------------------protocol end----------------------------"
@@ -274,6 +306,21 @@ def printNet(network,numIter,outFile):
     for idx,router in network.items():
         outFile.write(router.toString(len(network)))
         print (router.toString(len(network)))
+
+def printDelay(outFile, delay, protocol):
+    out = "Convergence Delay:%d"%delay
+    outFile.write(out)
+    print protocol + ": " + out
+def increaseEventDelay(basicChange, splitChange, poisonChange ,eventDelay):
+    if len(eventDelay) == 0:
+        return;
+    if basicChange:
+        eventDelay['basic'] += 1
+    if splitChange:
+        eventDelay['split'] += 1
+    if poisonChange:
+        eventDelay['poison'] += 1
+    return eventDelay
 
 def programStart(argv):
     if (len(argv)!=4):
@@ -300,42 +347,83 @@ def programStart(argv):
         reverse = open("output-poison-reversed.txt",'w')
 
 #start of the first ALGORITHM
+
+    # initialize all useful values
     numIterations = 0
     converged = False
     eventlist_counter = 0
+    eventDelay={}
+    OverallchangedBasic = False
+    OverallchangedSplit = False
+    OverallchangedPoison = False
 
     while not converged or numIterations <= max_event:
-
-
         event_trigger = False
         if (numIterations in roundlist):
+            roundNum = eventlist[eventlist_counter][0]
             r1 = eventlist[eventlist_counter][1]
             r2 = eventlist[eventlist_counter][2]
             newCost = eventlist[eventlist_counter][3]
             if newCost != -1:
                 netWorkbasic[r1].addAdj(r2,newCost)
                 netWorkbasic[r2].addAdj(r1,newCost)
+                netWorksplit[r1].addAdj(r2,newCost)
+                netWorksplit[r2].addAdj(r1,newCost)
+                netWorkpoison[r1].addAdj(r2,newCost)
+                netWorkpoison[r2].addAdj(r1,newCost)
             else:
                 netWorkbasic[r1].rmAdj(r2)
                 netWorkbasic[r2].rmAdj(r1)
+                netWorksplit[r1].rmAdj(r2)
+                netWorksplit[r2].rmAdj(r1)
+                netWorkpoison[r1].rmAdj(r2)
+                netWorkpoison[r2].rmAdj(r1)
             print r1, r2, newCost
             eventlist_counter+=1
             event_trigger = True
+
+            # initialize eventDelay with value = 0
+            eventDelay['basic'] = 0
+            eventDelay['split'] = 0
+            eventDelay['poison'] = 0
+
             converged = False
+
             # recalculate dv for r1 and r2
             print "--------------------------recalculate-----------------------------"
             calculateDV(netWorkbasic,netWorkbasic[r1])
             calculateDV(netWorkbasic,netWorkbasic[r2])
+            calculateDV(netWorksplit,netWorksplit[r1], split=True)
+            calculateDV(netWorksplit,netWorksplit[r2], split=True)
+            calculateDV(netWorkpoison,netWorkpoison[r1], poison=True)
+            calculateDV(netWorkpoison,netWorkpoison[r2], poison=True)
             print "--------------------------finish recalculation--------------------"
 
         OverallchangedBasic = routingProtocol(netWorkbasic, numIterations, basicF, flag)
-        print "overall changed? ", OverallchangedBasic
-        if not OverallchangedBasic:
+        OverallchangedSplit = routingProtocol(netWorksplit, numIterations, splitF, flag,split=True)
+        OverallchangedPoison = routingProtocol(netWorkpoison, numIterations, reverse, flag, poison=True)
+        print "overall changed basic: ", OverallchangedBasic
+        print "overall changed split: ", OverallchangedSplit
+        print "overall changed poison: ", OverallchangedPoison
+
+        if not (OverallchangedBasic or OverallchangedPoison or OverallchangedSplit):
             converged = True
 
         numIterations += 1
+        # if there are any value in event delay, we increase the iteration number in event delay
+        increaseEventDelay(OverallchangedBasic,OverallchangedPoison,OverallchangedSplit,eventDelay)
+
+    for protocol,delay in eventDelay.items():
+        if protocol == "basic":
+            printDelay(basicF,delay, protocol)
+        elif protocol == "poison":
+            printDelay(reverse, delay, protocol)
+        else:
+            printDelay(splitF, delay, protocol)
 
     basicF.close()
+    splitF.close()
+    reverse.close()
 
 if __name__ == "__main__":
     programStart(sys.argv)
