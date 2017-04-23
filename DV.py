4a0b95e33bd32eff0950f6bbf9bc74acbf79d11a
mxie33 <mxie33@gatech.edu>
2017-04-22 23:30:21 -0400

    project finished

--- DV.py
+++ DV.py
@@ -3,59 +3,129 @@ from string import *
 import sys
 from collections import defaultdict
 from copy import deepcopy
+"""
+Author: Yang Yang, Mingjun Xie, Yufeng Wang
+Version 1.0
 
+Code for Project 3
+"""
 class Router(object):
     def __init__(self,num):
+        """Function to initialize a Router
+
+        Args:
+            num: number of the router
+
+        Returns:
+            null
+
+        """
         self.num = num
         self.AdjList = []
         self.interation = 0
         self.costList = {self.num:0}
         self.destTable = {}
-    #def getDist(self,dest):
-    #    return self.destRoutes[dest]
-    #def addRoute(self,dest,dist):
-    #    self.destRoutes[dest]=dist
-    def addAdj(self,dest,cost):
-        self.AdjList.append(dest)
-        self.costList[dest]=cost
 
     def addAdj(self,dest,cost):
+        """Function to add an adjacent to the router
+
+        Args:
+            dest: number of the adjacent router
+            cost: cost to that router
+
+        Returns:
+            null
+
+        """
         if (dest not in self.AdjList):
             self.AdjList.append(dest)
         self.updateCost(dest,cost)
     def rmAdj(self, dest):
+        """Function to remove the adjacent
+
+        Args:
+            dest: number of the adjacent
+
+        Returns:
+            null
+
+        """
         self.AdjList = [x for x in self.AdjList if x != dest]
         self.updateCost(dest,float('inf'))
+        if self.destTable[dest][0] == dest:
+            self.destTable[dest] = [-1,float('inf'),-1]
 
     def updateCost(self,dest,cost):
+        """Function to update the cost to an adjacent
+
+        Args:
+            dest: number of the adjacent
+            cost: new cost
+
+        Returns:
+            null
+
+        """
         self.costList[dest] = cost
+
     def iniDestTable(self,numOfRouters):
+        """Function to initialize the router table
+
+        Args:
+            numOfRouters: total number of all routers
+
+        Returns:
+            null
+
+        """
         for i in range (1,numOfRouters+1):
             if i == self.num:
-                #print "ini self"
                 self.destTable[i] = [self.num,0,0]
-                #print self.destTable[i]
             elif i in self.AdjList:
-                #print "ini adj"
                 self.destTable[i] = [i,self.costList[i],1]
-                #print self.destTable[i]
             else:
-                #print "unknown"
                 self.destTable[i] = [-1,float('inf'),-1]
-
-                #print self.destTable[i]
-            #print "finish dest ini\n"
     def toString(self,numOfRouters):
+        """Function to print the table of the router
+
+        Args:
+             numOfRouters: total number of all routers
+
+        Returns:
+            null
+
+        """
         out = "%d\t"%self.num
         for i in range(1,numOfRouters+1):
             out += "%d,%d \t"%(self.destTable[i][0],self.destTable[i][2])
         out += "\n"
         return out
     def updateDest(self,dest,nextHop,distance,numHops):
+        """Function to update the router table
+
+        Args:
+            dest: the number of the router to update
+            nextHop: new nextHop
+            distance:new distance
+            numHops: new number of Hops
+
+        Returns:
+            null
+
+        """
         self.destTable[dest] = [nextHop,distance,numHops]
 
 # The method to initialize all routers in the network
 def iniRouter(routerFile):
+    """Function to initialze all Routers
+
+        Args:
+            routerFile: the router File
+
+        Returns:
+            network: the network of all routers
+
+        """
     try:
         RouteF = open(routerFile,'r')
     except:
@@ -63,26 +133,33 @@ def iniRouter(routerFile):
         sys.exit()
     netWork ={}
     ttlRNum = int(RouteF.readline())
-    print "total Number of router", ttlRNum
     for i in range(1, ttlRNum + 1):
         tempR = Router(i)
         netWork[i] = tempR
     l =  RouteF.readline()
     while l:
-        #print l
         r1,r2,cost = l.split()
         netWork[int(r1)].addAdj(int(r2),int(cost))
         netWork[int(r2)].addAdj(int(r1),int(cost))
         l =  RouteF.readline()
 
-    #print "add cost finished"
 
     for i in range(1,ttlRNum + 1):
-        #print "ini router destance",i
         netWork[i].iniDestTable(ttlRNum)
     return netWork
 
 def iniEvent(eventFile):
+    """Function the initialize all events
+
+        Args:
+            eventFile: eventFile
+
+        Returns:
+            eventlist: list of all events
+            roundlist: list of all rounds which have event
+            final_event: the round of the final event
+
+        """
     eventlist = []
     roundlist = []
     try:
@@ -107,7 +184,22 @@ def iniEvent(eventFile):
 
 # calculateDV method to calculate a single source routers distance vectors
 def calculateDV(oldnetWork, netWork,srcRouter, poison=False, split=False):
+    """Function the calculate the distance vector
+
+        Args:
+            oldnetWork: the current network
+            netWork: the network to be updated
+            srcRouter:the surce router
+            poison: if use poison algorithm
+            split: if use split algorithm
+
+        Returns:
+            overallChanged: if the table changed
+            infinity: if count to infinity happend in this round
+
+        """
     Overallchanged = False
+    infinity=False
     for destNum,destRouter in netWork.items():
         if destNum == srcRouter.num:
             continue
@@ -116,23 +208,18 @@ def calculateDV(oldnetWork, netWork,srcRouter, poison=False, split=False):
 
         # check all neighbours if they can reach destination
         for adjNum in srcRouter.AdjList:
-            #print "new network:", netWork[adjNum].destTable[destNum], "old network: ", oldnetWork[adjNum].destTable[destNum]
-            #print netWork[adjNum].destTable[destNum] == oldnetWork[adjNum].destTable[destNum]
+
             adjCost = srcRouter.costList[adjNum]
             adjDest = oldnetWork[adjNum].destTable[destNum][1]
             adjNexthop = oldnetWork[adjNum].destTable[destNum][0]
             adjNumHops = oldnetWork[adjNum].destTable[destNum][2]
-            #print "adjNextHop:", adjNexthop, "src:", srcRouter.num
             if oldnetWork[adjNum].destTable[destNum][0] == srcRouter.num:
                 if poison:
-                    print "poison (sending infinity as distance)!", "src:", srcRouter.num, "nexthop:", adjNum,"dest:", destNum, "new distance:", adjDest + adjCost, "num hops:", adjNumHops+1
                     adjDest = float('inf')
                 elif split:
-                    print "split(not sending distance vector)!", "src:", srcRouter.num, "nexthop:", adjNum,"dest:", destNum, "new distance:", adjDest + adjCost, "num hops:", adjNumHops+1
-                    # if split just don't send anything to that router
                     continue
                 else:
-                    #basic protocol
+                    
                     pass
 
             disFromAdj = adjDest + adjCost
@@ -142,68 +229,107 @@ def calculateDV(oldnetWork, netWork,srcRouter, poison=False, split=False):
                 tempCost = disFromAdj
                 nextHop = adjNum
                 numHops = adjNumHops+1
-
+        if tempCost == float('inf'):
+            nextHop = -1
+            numHops = -1
         if tempCost != minCost:
-            print "update!", "src:", srcRouter.num, "nexthop:", nextHop,"dest:", destNum, "new distance:", tempCost,"num hops:", numHops 
             Overallchanged = True
             srcRouter.updateDest(destNum,nextHop,tempCost,numHops)
 
-        if tempCost > 100:
-            print "Count to infinity problem! End iteration."
-            sys.exit()
+        if numHops > 100:
+            
+            infinity = True
+            Overallchanged = False
+            return Overallchanged,infinity
+            
 
-    return Overallchanged
+    return Overallchanged,infinity
 
 # choosing a routing protocol
 # default is not recalculate and is basic protocol
-def routingProtocol(netWork, numIterations, basicF, flag,poison=False, split=False):
+def routingProtocol(netWork, numIterations, outF, flag,poison=False, split=False):
+    """Function the calculate DV for all routers
+
+        Args:
+            netWork: the netWork to update
+            numIterations: the round number
+            outF: the file to print to
+            flag: the print out mode
+            poison: if use poison algorithm
+            split: if use split algorithm
+
+        Returns:
+            overallChanged: if the table changed
+            infinity: if count to infinity happend in this round
+
+        """
     #basic if poison == false and split == false
     Overallchanged = False
-    if not (poison or split):
-        print "basic protocol"
-    elif(poison):
-        print "poison reversed protocol"
-    else:
-        print "split horizon protocol"
     if flag == 1:
-        printNet(netWork,numIterations,basicF)
+        printNet(netWork,numIterations,outF)
 
     # iteration all source router
-    print "--------------------------protocol start--------------------------"
+    
     oldnetWork = deepcopy(netWork)
-    for srcNum,srcRouter in netWork.items():
+    for srcRouter in netWork.values():
         # check distance vector for every destination
-        localChanged = calculateDV(oldnetWork,netWork,srcRouter,poison=poison,split=split)
+        localChanged,infinity = calculateDV(oldnetWork,netWork,srcRouter,poison,split)
         if localChanged:
             Overallchanged = True
-    print "--------------------------protocol end----------------------------"
-    return Overallchanged
+        if infinity:
+            outF.write("Count to infinity problem! End iteration.\n")
+            break
 
-def printNet(network,numIter,outFile):
-    outFile.write("Round: %d\n"%numIter)
-    print ("Round: %d"%numIter)
-    for idx,router in network.items():
+    return Overallchanged,infinity
+
+def printNet(network,numIter,outFile,flag=1):
+    """Function to print the table to the file
+
+        Args:
+            network:current network
+            numIter: number of rounds
+            outFile: the file to print to
+            flag: the printing mode
+
+        Returns:
+            none
+
+        """
+    if flag:
+        outFile.write("Round: %d\n"%numIter)
+   
+    for router in network.values():
         outFile.write(router.toString(len(network)))
-        print (router.toString(len(network)))
+  
 
-def printDelay(outFile, delay, protocol):
-    out = "Convergence Delay:%d"%delay
-    outFile.write(out)
-    print protocol + ": " + out
+def printDelay(outFile, delay, infinity):
+    """Function to print convergence delay
 
-def increaseEventDelay(basicChange, splitChange, poisonChange ,eventDelay):
-    if len(eventDelay) == 0:
-        return;
+        Args:
+            outFile: the file to print to
+            delay: convergence delay
+            infinity: if count to infinity happened
+
+        Returns:
+            none
+
+        """
+    if infinity:
+        outFile.write("Convergence Delay:N/A")
     else:
-        if basicChange:
-            eventDelay['basic'] += 1
-        if splitChange:
-            eventDelay['split'] += 1
-        if poisonChange:
-            eventDelay['poison'] += 1
-    return eventDelay
+        outFile.write("Convergence Delay:%d"%delay)
+   
 
 def programStart(argv):
+    """Function of the enter of the program, conduct three algorithms in sequence
+
+        Args:
+            argv: command input
+
+        Returns:
+            none
+
+        """
     if (len(argv)!=4):
         print("Invalid arguments")
         sys.exit()
@@ -221,25 +347,44 @@ def programStart(argv):
     if flag == 1:
         basicF = open("output-detailed-basic.txt",'w')
         splitF = open("output-detailed-split-horizon.txt",'w')
-        reverse = open("output-detailed-poison-reversed.txt",'w')
+        poisonF = open("output-detailed-poison-reversed.txt",'w')
+        print ("output-detailed-basic.txt|output-detailed-split-horizon.txt|output-detailed-poison-reversed.txt")
     else:
         basicF = open("output-basic.txt",'w')
         splitF = open("output-split-horizon.txt",'w')
-        reverse = open("output-poison-reversed.txt",'w')
+        poisonF = open("output-poison-reversed.txt",'w')
+        print ("output-basic.txt|output-split-horizon.txt|output-poison-reversed.txt")
+    mainLoop(basicF,netWorkbasic,eventlist,roundlist,max_event,flag)
+    mainLoop(splitF,netWorksplit,eventlist,roundlist,max_event,flag,False,True)
+    mainLoop(poisonF,netWorkpoison,eventlist,roundlist,max_event,flag,True,False)
+    
+    
 
-#start of the first ALGORITHM
+def mainLoop(outF,netWork,eventlist,roundlist,max_event,flag,poison=False,split=False):
+    """Function to calculate a specific algorithm
 
-    # initialize all useful values
-    numIterations = 0
+        Args:
+            outF: the file to print to
+            netWork: the network to calculate
+            eventlist: list of events
+            roundlist: list of event rounds
+            max_event: the final event round
+            flag: printing mode
+            poison: if use poison alorithm
+            split: if use split mode
+
+        Returns:
+            none
+
+        """
+    numIterations = 1
     converged = False
     eventlist_counter = 0
-    eventDelay={}
-    OverallchangedBasic = True
-    OverallchangedSplit = True
-    OverallchangedPoison = True
-
+    eventDelay=0
+    Overallchanged = True
+    infinity = False
     while not converged or numIterations <= max_event:
-        event_trigger = False
+
         if (numIterations in roundlist):
             # if event triggered, we only do cost updates and r1,r2 dv updates
             roundNum = eventlist[eventlist_counter][0]
@@ -247,73 +392,47 @@ def programStart(argv):
             r2 = eventlist[eventlist_counter][2]
             newCost = eventlist[eventlist_counter][3]
             if newCost != -1:
-                netWorkbasic[r1].addAdj(r2,newCost)
-                netWorkbasic[r2].addAdj(r1,newCost)
-                netWorksplit[r1].addAdj(r2,newCost)
-                netWorksplit[r2].addAdj(r1,newCost)
-                netWorkpoison[r1].addAdj(r2,newCost)
-                netWorkpoison[r2].addAdj(r1,newCost)
+                netWork[r1].addAdj(r2,newCost)
+                netWork[r2].addAdj(r1,newCost)
             else:
-                netWorkbasic[r1].rmAdj(r2)
-                netWorkbasic[r2].rmAdj(r1)
-                netWorksplit[r1].rmAdj(r2)
-                netWorksplit[r2].rmAdj(r1)
-                netWorkpoison[r1].rmAdj(r2)
-                netWorkpoison[r2].rmAdj(r1)
-            print "event:", "router1:", r1,"router2:", r2,"newcost:", newCost
+                netWork[r1].rmAdj(r2)
+                netWork[r2].rmAdj(r1)
+           
             eventlist_counter+=1
-            event_trigger = True
+           
 
             # initialize eventDelay with value = 0
-            eventDelay['basic'] = 0
-            eventDelay['split'] = 0
-            eventDelay['poison'] = 0
+            eventDelay = 0
 
-            OverallchangedBasic = True
-            OverallchangedSplit = True
-            OverallchangedPoison = True
+            Overallchanged = True
             converged = False
 
             # recalculate dv for r1 and r2
-            print "------------------------round ", numIterations," recalculate---------------------"
-            calculateDV(deepcopy(netWorkbasic),netWorkbasic,netWorkbasic[r1])
-            calculateDV(deepcopy(netWorkbasic),netWorkbasic,netWorkbasic[r2])
-            calculateDV(deepcopy(netWorksplit),netWorksplit,netWorksplit[r1], split=True)
-            calculateDV(deepcopy(netWorksplit),netWorksplit,netWorksplit[r2], split=True)
-            calculateDV(deepcopy(netWorkpoison),netWorkpoison,netWorkpoison[r1], poison=True)
-            calculateDV(deepcopy(netWorkpoison),netWorkpoison,netWorkpoison[r2], poison=True)
-            print "--------------------------finish recalculation--------------------"
+           
+            if flag == 1:
+                printNet(netWork,numIterations,outF)
+            calculateDV(deepcopy(netWork),netWork,netWork[r1],poison,split)
+            calculateDV(deepcopy(netWork),netWork,netWork[r2],poison,split)
+            
+           
         else:
             # if not event trigger we are normally doing routing protocols
-            OverallchangedBasic = routingProtocol(netWorkbasic, numIterations, basicF, flag)
-            OverallchangedSplit = routingProtocol(netWorksplit, numIterations, splitF, flag,split=True)
-            OverallchangedPoison = routingProtocol(netWorkpoison, numIterations, reverse, flag, poison=True)
-            print "overall changed basic: ", OverallchangedBasic
-            print "overall changed split: ", OverallchangedSplit
-            print "overall changed poison: ", OverallchangedPoison
-            print "------------------------round ", numIterations, " finished------------------------" 
-            if not (OverallchangedBasic or OverallchangedPoison or OverallchangedSplit):
-                converged = True
-
-        numIterations += 1
-        # if there are any value in event delay, we increase the iteration number in event delay
-        increaseEventDelay(OverallchangedBasic,OverallchangedPoison,OverallchangedSplit,eventDelay)
-
-
-    if flag==0:
-        printNet(netWorkbasic,numIterations-1,basicF)
-        printNet(netWorksplit,numIterations-1,splitF)
-        printNet(netWorkpoison,numIterations-1,reverse)
-    for protocol,delay in eventDelay.items():
-        if protocol == "basic":
-            printDelay(basicF,delay, protocol)
-        elif protocol == "poison":
-            printDelay(reverse, delay, protocol)
+            Overallchanged,infinity = routingProtocol(netWork, numIterations, outF, flag,poison,split) 
+        if not Overallchanged:
+            converged = True
         else:
-            printDelay(splitF, delay, protocol) 
-    basicF.close()
-    splitF.close()
-    reverse.close()
+            eventDelay+= 1
+        numIterations += 1
+        if infinity:
+            break
+        # if there are any value in event delay, we increase the iteration number in event delay
+        
+
+
+    if flag == 0:
+        printNet(netWork,numIterations-1,outF,flag)
+    printDelay(outF,eventDelay,infinity)
+    outF.close()
 
 if __name__ == "__main__":
     programStart(sys.argv)
